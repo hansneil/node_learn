bcc0a102d25f5b3a7414e611a933c95048a02755
Yu, Xianbo <yxbneil@163.com>
2015-12-17 16:57:57 +0800

    modify ch6

--- ch6.js
+++ ch6.js
@@ -49,7 +49,7 @@ var fs = require("fs");
  * 特点：将字符串或缓冲区的全部内容写入一个文件
  */
 
-/*var config = {
+var config = {
 	maxFiles: 20,
 	maxConnection: 15,
 	rootPath: "/webroot"
@@ -63,7 +63,7 @@ fs.writeFile('config.txt', configTxt, options, function(err){
 	} else {
 		console.log("config saved");
 	}
-});*/
+});
 
 //同步写入文件 writeSync(fd, data, offset, length, position) 注意打开和关闭文件
 /*
@@ -74,14 +74,14 @@ fs.writeFile('config.txt', configTxt, options, function(err){
  * position: 指定文件中开始写入的位置，若为NULL，表示从当前位置开始
  */
 
-/*var veggieTray = ["carrots", "celery", "olives"];
+var veggieTray = ["carrots", "celery", "olives"];
 fd = fs.openSync('veggie.txt', 'w');
 while (veggieTray.length) {
 	veggie = veggieTray.shift() + " ";
 	var bytes = fs.writeSync(fd, veggie, null, null, null);
 	console.log("wrote %s %dbytes", veggie, bytes);
 }
-fs.closeSync(fd);*/
+fs.closeSync(fd);
 
 //异步写入文件 write(fd, data, offset, length, position, callback) callback必须包含err和bytes
 /*
@@ -96,7 +96,7 @@ fs.closeSync(fd);*/
  *    callback接受两个参数，err 和 bytes
  */
 
-/*var fruitBowl = ["apple", "orange", "banana", "grapes"];
+var fruitBowl = ["apple", "orange", "banana", "grapes"];
 function writeFruit(fd) {
 	if (fruitBowl.length) {
 		var fruit = fruitBowl.shift() + " ";
@@ -115,7 +115,7 @@ function writeFruit(fd) {
 
 fs.open('fruit.txt', 'w', function(err, fd){
 	writeFruit(fd);
-});*/
+});
 
 //流式文件写入 fs.createWriteStream创建Writable对象，之后就可以用write写入了
 /*
@@ -125,7 +125,7 @@ fs.open('fruit.txt', 'w', function(err, fd){
  * 写入方式：标准的流式write(buffer)写入，写入最后用end结束
  */
 
-/*var grains = ["wheat", "rice", "oats"];
+var grains = ["wheat", "rice", "oats"];
 var options = {encoding: "utf8", flag: "w"};
 var fileWriteStream = fs.createWriteStream('grains.txt', options);
 fileWriteStream.on("close", function(){
@@ -137,7 +137,7 @@ while (grains.length) {
 	fileWriteStream.write(data);
 	console.log("write: %s", data);
 }
-fileWriteStream.end();*/
+fileWriteStream.end();
 
 /*
  * 3.读取文件
@@ -149,7 +149,7 @@ fileWriteStream.end();*/
  */
 //简单文件读取 readFile(path, [options], callback)/readFileSync(path, [options])
 
-/*var options = {encoding: 'utf8', flag: 'r'}; //encoding | mode | flag
+var options = {encoding: 'utf8', flag: 'r'}; //encoding | mode | flag
 fs.readFile('config.txt', options, function(err, data){
 	if (err) {
 		console.log("failed to open config file");
@@ -161,11 +161,11 @@ fs.readFile('config.txt', options, function(err, data){
 		console.log("maxConnection: ", dataObj.maxConnection);
 		console.log("rootPath: ", dataObj.rootPath);
 	}
-});*/
+});
 
 //同步读取文件 readSync(fd, buffer, offset, length, position)
 
-/*fd = fs.openSync("veggie.txt", 'r');
+fd = fs.openSync("veggie.txt", 'r');
 var grains = "";
 do {
 	var buf = new Buffer(5);
@@ -175,14 +175,14 @@ do {
 	grains += buf.toString();
 }while (bytes > 0);
 fs.closeSync(fd);
-console.log("Grains: ", grains);*/
+console.log("Grains: ", grains);
 
 //异步读取文件 read(fd, buffer, offset, length, position, callback)
 /*
  * 为确保读取数据的顺序，可以将读操作嵌套在上一次的读取操作中
  */
 
-/*function readGrains(fd, grains) {
+function readGrains(fd, grains) {
 	var buf = new Buffer(5);
 	buf.fill();
 	fs.read(fd, buf, 0, 5, null, function(err, bytes, data){
@@ -201,7 +201,7 @@ console.log("Grains: ", grains);*/
 
 fs.open("fruit.txt", 'r', function(err, fd){
 	readGrains(fd, "");
-});*/
+});
 
 //流式文件读取 createReadStream(path, [options])
 /*
@@ -209,7 +209,6 @@ fs.open("fruit.txt", 'r', function(err, fd){
  * 打开文件后，可以通过readable事件，read()请求或者data事件轻松地将数据读出
  */
 
-/*
 var options = {encoding: 'utf8', flag: 'r'};
 var fileReadStream = fs.createReadStream("grains.txt", options);
 fileReadStream.on("data", function(chunk){
@@ -219,7 +218,6 @@ fileReadStream.on("data", function(chunk){
 fileReadStream.on("close", function(){
 	console.log("file closed");
 });
-*/
 
 /*
  * 4.其他文件系统任务
@@ -233,12 +231,12 @@ fileReadStream.on("close", function(){
  * 同步调用时是一个返回值
  */
 
-/*fs.exists("config.txd", function(exist){
+fs.exists("config.txd", function(exist){
 	console.log(exist ? "Path exists" : "Path does not exists");
 });
 
 var exist = fs.existsSync("config.txt");
-console.log(exist ? "Path exists" : "Path does not exists");*/
+console.log(exist ? "Path exists" : "Path does not exists");
 
 /*
  * 4.2 获取文件信息
@@ -246,7 +244,7 @@ console.log(exist ? "Path exists" : "Path does not exists");*/
  * fs.statSync(path)
  */
 
-/*fs.stat("config.txt", function(err, stats){
+fs.stat("config.txt", function(err, stats){
 	if (err) {
 		console.log("Path not exists!!!");
 	} else {
@@ -255,7 +253,7 @@ console.log(exist ? "Path exists" : "Path does not exists");*/
 		console.log(stats.isDirectory());
 		console.log(stats.isSocket());
 	}
-});*/
+});
 
 /*
  * 4.3 列出文件
@@ -263,7 +261,7 @@ console.log(exist ? "Path exists" : "Path does not exists");*/
  * fs.readdirSync(path)
  */
 
-/*var path = require("path");
+var path = require("path");
 function WalkDirs(dirPath) {
 	console.log(dirPath);
 	fs.readdir(dirPath, function(err, entries){
@@ -281,7 +279,7 @@ function WalkDirs(dirPath) {
 		}
 	});
 }
-WalkDirs(".");*/
+WalkDirs(".");
 
 /*
  * 4.4 删除文件
@@ -289,9 +287,9 @@ WalkDirs(".");*/
  * fs.unlinkSync(path)
  */
 
-/*fs.unlink("new.txt", function(err){
+fs.unlink("new.txt", function(err){
 	console.log(err ? "del failed" : "del");
-});*/
+});
 
 /*
  * 4.5 截断文件
@@ -299,31 +297,29 @@ WalkDirs(".");*/
  * fs.truncateSync(path, len)
  */
 
-/*fs.truncate("new.txt", function(err){
+fs.truncate("new.txt", function(err){
 	console.log(err ? "truncate failed" : "truncated");
-});*/
+});
 
 /*
  * 4.6 创建删除目录
  */
-/*
+
 fs.mkdir("./data", function(err){
 	fs.mkdir("./data/folderA", function(err){
 		console.log("complete");
 	});
-});*/
+});
 
-/*
 fs.rmdir("./data/folderA", function(err){
 	fs.rmdir("./data", function(err){
 		console.log("recovered");
 	});
-});*/
+});
 
-/*
 fs.rename("config.txt", "grains.txt", function(err){
 	console.log(err ? "rename failed" : "file renamed");
-});*/
+});
 
 fs.watchFile("grains.txt", {persistent: true, interval: 5000}, function(curr, prev){
 	console.log("grains.txt modified at: " + curr.mtime);
